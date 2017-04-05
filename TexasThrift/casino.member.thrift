4d3133cc5b26cfd70bc787ab9da5fe3636bd6b9e
fangzhongwei <zhongwei.fang@56qq.com>
2017-04-05 12:06:20 +0800

    texas

--- TexasThrift/casino.member.thrift
+++ TexasThrift/casino.member.thrift
@@ -2,16 +2,14 @@ namespace java com.jxjxgo.casino.memberber.rpc
 #@namespace scala com.jxjxgo.casino.memberber.rpc
 
 struct ThirdProfile {
-    1: i16 plateform = 0,
-    2: string _id = "",
-    3: string _name = "",
-    4: string _alias = "",
-    5: optional list<byte> _smallPhoto,
-    6: optional list<byte> _bigPhoto,
-    7: bool _hasIconImage = false,
-    8: bool _hasHiResImage = false,
-    9: string _iconImageUrl = "",
-    10: string _hiResImageUrl = "",
+    1: i16 platform = 0,
+    2: string id = "",
+    3: string name = "",
+    4: string alias = "",
+    5: string avatarUrl = "",
+    6: string avatarMd5 = "",
+    7: string photoUrl = "",
+    8: string photoMd5 = "",
 }
 
 struct MemberBaseResponse {
@@ -21,17 +19,21 @@ struct MemberBaseResponse {
 struct MemberResponse {
     1: string code = "0",
     2: i64 memberId = 0,
-    3: i16 status = 0,
-    4: string nickName = "",
-    5: string avatarUrl = "",
-    6: i64 gmtCreate = 0,
-    7: i64 gmtUpdate = 0,
+    3: i16 platform = 0,
+    4: i16 status = 0,
+    5: string nickName = "",
+    6: string avatarUrl = "",
+    7: string avatarMd5 = "",
+    8: string photoUrl = "",
+    9: string photoMd5 = "",
+    10: i64 gmtCreate = 0,
+    11: i64 gmtUpdate = 0,
 }
 
 service MemberEndpoint {
-    MemberBaseResponse register(1: string traceId, 2: ThirdProfile thirdProfile),
+    MemberResponse register(1: string traceId, 2: ThirdProfile thirdProfile),
     MemberResponse getMemberById(1: string traceId, 2: i64 memberId),
-    MemberResponse getMemberByThirdpartId(1: string traceId, 2: i16 plateform, string thirdpartId),
+    MemberResponse getMemberByThirdpartId(1: string traceId, 2: i16 platform, string thirdpartId),
     MemberBaseResponse updateMemberStatus(1: string traceId, 2: i64 memberId, 3: i16 status),
     MemberBaseResponse updateNickName(1: string traceId, 2: i64 memberId, 3: string nickName),
 }
